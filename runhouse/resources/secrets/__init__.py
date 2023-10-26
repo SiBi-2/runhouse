3d895801624e76545d6dd4f72ec83925c9a175ad
Caroline <chen.caroline12@gmail.com>
2023-10-26 18:32:30 -0400

    [wip] add env and cluster secrets

--- runhouse/resources/secrets/__init__.py
+++ runhouse/resources/secrets/__init__.py
@@ -1,4 +1,5 @@
+from .env_secret import EnvSecret
 from .provider_secrets.provider_secret import ProviderSecret
 from .provider_secrets.ssh_secret import SSHSecret
 from .secret import Secret
-from .secret_factory import provider_secret, secret
+from .secret_factory import cluster_secret, env_secret, provider_secret, secret
